e4f3cb99defed5928c4e9a4b438e5e5f8e387ea7
18033150689 <3946905217@qq.com>
2026-03-08 21:49:30 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,84 @@
+// 美团外卖红包配置文件
+// 自动生成时间：2026/3/8 21:35:34
+
+const siteConfig = {
+  "siteTitle": "饿了么外卖红包",
+  "bgColor": "#3f95ff",
+  "header": {
+    "enabled": true,
+    "logoUrl": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/6RCU/1308X1280/565df7546323c43a291c5f6edb92f35c.jpg/webp",
+    "text": "饿了么",
+    "textColor": "#000000",
+    "fontSize": "24px",
+    "fontWeight": "bold",
+    "align": "center",
+    "logoPosition": "left",
+    "logoSize": "40px"
+  },
+  "carousel": [
+    {
+      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/6Bzm/750X650/%E5%93%88%E6%A0%B9%E8%BE%BE%E6%96%AF.jpeg/webp",
+      "title": "哈根达斯",
+      "subtitle": "满50减8可叠加！",
+      "link": "https://mp.weixin.qq.com/s/Hv918eS5p5dRwvHFF6tGuw"
+    },
+    {
+      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/xPAO/750X650/%E4%B9%90%E4%B9%90%E8%8C%B6.jpeg/webp",
+      "title": "乐乐茶",
+      "subtitle": "满30减8可叠加",
+      "link": "https://mp.weixin.qq.com/s/4W8U2asGg8umfGWJ_1O6-g"
+    }
+  ],
+  "stepsType": "image",
+  "stepsText": "先领券 → 再下单 → 享优惠",
+  "stepsColor": "#FF6B00",
+  "mainButton": {
+    "text": "天天领红包",
+    "color": "#005aad",
+    "textColor": "#FFFFFF",
+    "link": "https://mp.weixin.qq.com/s/Rb0a2l7D9A3buMtXzw0Zjg"
+  },
+  "subButtonsEnabled": true,
+  "subButtons": {
+    "button1": {
+      "text": "消费日专属",
+      "desc": "城市大额红包城市通用消费红包",
+      "link": "https://mp.weixin.qq.com/s/kY_6eWWm577-2f4pL-01Wg"
+    },
+    "button2": {
+      "text": "限时快闪",
+      "desc": "最高领18元红包",
+      "link": "https://mp.weixin.qq.com/s/Qt2I5Ix1-N7zwkjXbHLCzQ"
+    }
+  },
+  "imageButtonsEnabled": true,
+  "imageButtons": [
+    {
+      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/vSJg/750X651/%E6%B1%89%E5%A0%A1%E7%8E%8B.jpeg/webp",
+      "text": "满40减6可叠加",
+      "link": "http://dpurl.cn/2IPwHM1z"
+    },
+    {
+      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/jRq4/750X649/%E5%8D%8E%E8%8E%B1%E5%A3%AB.jpeg/webp",
+      "text": "满25减4可叠加",
+      "link": "http://dpurl.cn/YzzSTMIz"
+    },
+    {
+      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/kPLQ/750X648/%E6%B0%B8%E5%92%8C%E5%A4%A7%E7%8E%8B.jpeg/webp",
+      "text": "满30减8可叠加",
+      "link": "http://dpurl.cn/HMGWpnKz"
+    }
+  ],
+  "tips": [
+    "1. 先领完上方所有的券，领完后返回此页面，点击“快速用券”-点去使用，即可使用券。",
+    "2. 饿了么，点外卖红包和加餐红包，同时领取。",
+    "3. 收货人手机号和登录手机号，必须一致才可用券。查看个人中心教程进行修改。",
+    "4. 美团一天可领3次，饿了么可以领两个(外卖红包和加餐红包)"
+  ],
+  "stepsImage": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/dl4F/626X135/1.png/webp"
+};
+
+// 导出配置
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = siteConfig;
+}
