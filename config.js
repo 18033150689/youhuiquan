8e92974cba70e1e52a48f5346eaf1f0d377c53da
18033150689 <3946905217@qq.com>
2026-03-08 22:46:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,84 +1,82 @@
-// 美团外卖红包配置文件
-// 自动生成时间：2026/3/8 21:35:34
+// config.js - 美团外卖红包配置文件
+// 将此文件上传到 GitHub 仓库
+// 链接格式：https://cdn.jsdelivr.net/gh/用户名/仓库名@分支名/config.js
 
 const siteConfig = {
-  "siteTitle": "饿了么外卖红包",
-  "bgColor": "#3f95ff",
-  "header": {
-    "enabled": true,
-    "logoUrl": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/6RCU/1308X1280/565df7546323c43a291c5f6edb92f35c.jpg/webp",
-    "text": "饿了么",
-    "textColor": "#000000",
-    "fontSize": "24px",
-    "fontWeight": "bold",
-    "align": "center",
-    "logoPosition": "left",
-    "logoSize": "40px"
-  },
-  "carousel": [
-    {
-      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/6Bzm/750X650/%E5%93%88%E6%A0%B9%E8%BE%BE%E6%96%AF.jpeg/webp",
-      "title": "哈根达斯",
-      "subtitle": "满50减8可叠加！",
-      "link": "https://mp.weixin.qq.com/s/Hv918eS5p5dRwvHFF6tGuw"
+    "siteTitle": "美团外卖红包",
+    "bgColor": "#FFD700",
+    "header": {
+        "enabled": true,
+        "logoUrl": "",
+        "text": "美团外卖红包",
+        "textColor": "#FF6B00",
+        "fontSize": "24px",
+        "fontWeight": "bold",
+        "align": "center",
+        "logoPosition": "left",
+        "logoSize": "50px"
     },
-    {
-      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/xPAO/750X650/%E4%B9%90%E4%B9%90%E8%8C%B6.jpeg/webp",
-      "title": "乐乐茶",
-      "subtitle": "满30减8可叠加",
-      "link": "https://mp.weixin.qq.com/s/4W8U2asGg8umfGWJ_1O6-g"
-    }
-  ],
-  "stepsType": "image",
-  "stepsText": "先领券 → 再下单 → 享优惠",
-  "stepsColor": "#FF6B00",
-  "mainButton": {
-    "text": "天天领红包",
-    "color": "#005aad",
-    "textColor": "#FFFFFF",
-    "link": "https://mp.weixin.qq.com/s/Rb0a2l7D9A3buMtXzw0Zjg"
-  },
-  "subButtonsEnabled": true,
-  "subButtons": {
-    "button1": {
-      "text": "消费日专属",
-      "desc": "城市大额红包城市通用消费红包",
-      "link": "https://mp.weixin.qq.com/s/kY_6eWWm577-2f4pL-01Wg"
+    "carousel": [
+        {
+            "image": "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=800&auto=format&fit=crop",
+            "title": "品质外卖",
+            "subtitle": "精选商家5折起",
+            "link": "https://meituan.com"
+        },
+        {
+            "image": "https://images.unsplash.com/photo-1565958011703-44f9829ba187?w=800&auto=format&fit=crop",
+            "title": "超值优惠",
+            "subtitle": "新人专享大礼包",
+            "link": "https://meituan.com"
+        },
+        {
+            "image": "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=800&auto=format&fit=crop",
+            "title": "限时特惠",
+            "subtitle": "满30减10元",
+            "link": "https://meituan.com"
+        }
+    ],
+    "mainButton": {
+        "text": "天天领红包",
+        "color": "linear-gradient(135deg, #FF6B00, #FF9800)",
+        "textColor": "#FFFFFF",
+        "link": "https://meituan.com"
     },
-    "button2": {
-      "text": "限时快闪",
-      "desc": "最高领18元红包",
-      "link": "https://mp.weixin.qq.com/s/Qt2I5Ix1-N7zwkjXbHLCzQ"
-    }
-  },
-  "imageButtonsEnabled": true,
-  "imageButtons": [
-    {
-      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/vSJg/750X651/%E6%B1%89%E5%A0%A1%E7%8E%8B.jpeg/webp",
-      "text": "满40减6可叠加",
-      "link": "http://dpurl.cn/2IPwHM1z"
+    "subButtonsEnabled": true,
+    "subButtons": {
+        "button1": {
+            "text": "品质会场(新)",
+            "desc": "精选商家5折起",
+            "link": "https://meituan.com"
+        },
+        "button2": {
+            "text": "超值券包",
+            "desc": "多张优惠券组合",
+            "link": "https://meituan.com"
+        }
     },
-    {
-      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/jRq4/750X649/%E5%8D%8E%E8%8E%B1%E5%A3%AB.jpeg/webp",
-      "text": "满25减4可叠加",
-      "link": "http://dpurl.cn/YzzSTMIz"
-    },
-    {
-      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/kPLQ/750X648/%E6%B0%B8%E5%92%8C%E5%A4%A7%E7%8E%8B.jpeg/webp",
-      "text": "满30减8可叠加",
-      "link": "http://dpurl.cn/HMGWpnKz"
-    }
-  ],
-  "tips": [
-    "1. 先领完上方所有的券，领完后返回此页面，点击“快速用券”-点去使用，即可使用券。",
-    "2. 饿了么，点外卖红包和加餐红包，同时领取。",
-    "3. 收货人手机号和登录手机号，必须一致才可用券。查看个人中心教程进行修改。",
-    "4. 美团一天可领3次，饿了么可以领两个(外卖红包和加餐红包)"
-  ],
-  "stepsImage": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/dl4F/626X135/1.png/webp"
+    "imageButtonsEnabled": true,
+    "imageButtons": [
+        {
+            "image": "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=400&auto=format&fit=crop",
+            "text": "免费领取",
+            "link": "https://meituan.com"
+        },
+        {
+            "image": "https://images.unsplash.com/photo-1565958011703-44f9829ba187?w=400&auto=format&fit=crop",
+            "text": "新人专享",
+            "link": "https://meituan.com"
+        },
+        {
+            "image": "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=400&auto=format&fit=crop",
+            "text": "限时特惠",
+            "link": "https://meituan.com"
+        }
+    ],
+    "tips": [
+        "1. 先领完上方所有的券，领完后返回此页面，点击对应按钮即可使用券。",
+        "2. 收货人手机号和登录手机号必须一致才可用券。",
+        "3. 美团一天可领3次，饿了么可以领两个红包。",
+        "4. 优惠券有效期以领取时显示为准。"
+    ]
 };
-
-// 导出配置
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = siteConfig;
-}
